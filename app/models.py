4243c6e860960861a30ddb6c3a3621a25dfd7641
Ari Alexander Tannahill Thomson <ari_alexander@outlook.com>
2026-07-15 00:39:24 +0100

    Commentary in  models page

--- app/models.py
+++ app/models.py
@@ -10,6 +10,10 @@ from flask import url_for
 from sqlalchemy import func, JSON
 from sqlalchemy.ext.mutable import MutableList
 
+
+# TODO: Readd email support
+# TODO: Add display names
+# TODO: Add leagues
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
